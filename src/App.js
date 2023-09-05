c02d602c8a94e4a01c52d9c50a7a4c824efc96d8
Muhammad Tehsin <tehsin118@gmail.com>
2023-09-05 16:25:22 +0500

    start up file update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import Home from "./page/home";
 
 function App() {
   return (
     <div className="App">
+      <h4 className="text-center mt-5 pt-5 ">
+        please update all dependencies by using <h2>npm update</h2>
+      </h4>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/page/home.js
+++ src/page/home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <input type="number" />
+    </div>
+  );
 };
 
 export default Home;
