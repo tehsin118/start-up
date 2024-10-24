54494daa145cac38ff42c5962d7a370779f52811
Muhammad Tehsin <tehsin118@gmail.com>
2024-10-24 15:46:34 +0500

    depened updated

--- src/components/home/hero.jsx
+++ src/components/home/hero.jsx
@@ -5,9 +5,19 @@ const Hero = () => {
   return (
     <>
       <div className="text-center ">
+        <ul>
+          <li>
+            <input type="radio" name="" id="" />
+          </li>
+        </ul>
         <h3 className="font-semibold mb-10">
           Setup your own color scheme and typography
         </h3>
+
+        <h4>
+          use <b> npx npm-check-updates -u </b> then <b>npm i</b> to update and
+          install all updated dependencies
+        </h4>
 
         <ul className="flex gap-3 m-auto justify-center mb-10">
           <li>
